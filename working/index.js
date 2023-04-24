fc39f501d9952edf747f8155960e4817c307cf33
Alek Novkovski <alek@greatcode2.com>
2023-04-24 17:28:18 +0200

    Add click handlers on nav buttons

---
+++ working/index.js
@@ -0,0 +1,10 @@
+const prevSlide = document.querySelector('.prev-slide-btn')
+const nextSlide = document.querySelector('.next-slide-btn')
+
+prevSlide.addEventListener('click', () => {
+    alert('left')
+})
+
+nextSlide.addEventListener('click', () => {
+    alert('right')
+})
