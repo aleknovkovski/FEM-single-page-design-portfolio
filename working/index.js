219d2583799666a37a7f75105ba44520c61ed747
Alek Novkovski <alek@greatcode2.com>
2023-04-24 18:44:34 +0200

    Swap first and last element on navigation

--- working/index.js
+++ working/index.js
@@ -1,11 +1,16 @@
+const portfolioSlider = document.querySelector('.portfolio-slider')
+
 const prevSlide = document.querySelector('.prev-slide-btn')
 const nextSlide = document.querySelector('.next-slide-btn')
-const slides = document.querySelectorAll('.slide')
 
 prevSlide.addEventListener('click', () => {
-    slides.forEach(slide => slide.style.transform = 'translateX(-560px)')
+    const slides = document.querySelectorAll('.slide')
+    portfolioSlider.removeChild(slides[0])
+    portfolioSlider.appendChild(slides[0])
 })
 
 nextSlide.addEventListener('click', () => {
-    slides.forEach(slide => slide.style.transform = 'translateX(560px)')
+    const slides = document.querySelectorAll('.slide')
+    portfolioSlider.removeChild(slides[slides.length-1])
+    portfolioSlider.insertBefore(slides[slides.length-1], portfolioSlider.firstChild, )
 })
